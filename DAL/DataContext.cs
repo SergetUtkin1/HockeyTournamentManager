8665167f9466222a5528ca779a88db81851197ea
SergeyUtkinEZ <sergey.uty.uty@gmail.com>
2023-04-01 20:56:51 +0300

    Prepare project for first migration

--- DAL/DataContext.cs
+++ DAL/DataContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,34 @@ using System.Threading.Tasks;
 
 namespace DAL
 {
-    internal class DataContext
+    internal class DataContext : DbContext
     {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+           => optionsBuilder.UseNpgsql(b => b.MigrationsAssembly("ConsoleInterface"));
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Goal>().ToTable(nameof(Goals));
+            modelBuilder.Entity<Infraction>().ToTable(nameof(Infractions));
+            modelBuilder.Entity<MatchStatistic>().ToTable(nameof(MatchStatistics));
+            modelBuilder.Entity<Penalty>().ToTable(nameof(Penalties));
+            modelBuilder.Entity<Player>().ToTable(nameof(Players));
+            modelBuilder.Entity<Team>().ToTable(nameof(Teams));
+            modelBuilder.Entity<TeamStatistic>().ToTable(nameof(TeamStatistics));
+        }
+
+        public DbSet<Goal> Goals => Set<Goal>();
+        public DbSet<Infraction> Infractions => Set<Infraction>();
+        public DbSet<Match> Matches => Set<Match>();
+        public DbSet<MatchStatistic> MatchStatistics => Set<MatchStatistic>();
+        public DbSet<Penalty> Penalties => Set<Penalty>();
+        public DbSet<Player> Players => Set<Player>();
+        public DbSet<Team> Teams => Set<Team>();
+        public DbSet<TeamStatistic> TeamStatistics => Set<TeamStatistic>();
     }
 }
